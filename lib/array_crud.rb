c0da6e8758605be0f934e2ed3731155c3da7636b
Megan Wong <mwong068@gmail.com>
2019-10-17 04:29:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  arrie = [1, 2, 3]
-  arrie.push(4)
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
